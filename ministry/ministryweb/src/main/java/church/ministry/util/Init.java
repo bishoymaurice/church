6aaf24a6cfcdb10654fb8a0e1d863f25cabb435f
bishoymaurice <bishoy.maurice@gmail.com>
2019-05-04 19:14:02 +0200

    unneeded project

--- ministry/ministryweb/src/main/java/church/ministry/util/Init.java
+++
@@ -1,9 +0,0 @@
-package church.ministry.util;
-
-import church.ministry.model.file.FileAccess;
-
-public class Init {
-	public static void initiate() {
-		FileAccess.createFolder("downloads");
-	}
-}
